34e4b3fbb5fc6a5f2b03510c770aa4f608bb8498
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-01-16 15:25:18 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Abby",
+    streetAddress: "11 Broadway",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  }
+  
+  const newEmployee = updateEmployeeWithKeyAndValue(
+    newEmployee,
+    "name",
+    "Alex"
+  );
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = { ...employee};
+
+    delete newEmployee.name;
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+
+    delete employee.name;
+
+    return employee;
+}
